9bcab7796d152c1292d10ab1e3bad6ad39669199
Pierre <misiuk.pierre@gmail.com>
2023-02-02 11:59:06 +0100

    modif q4

--- q4.c
+++ q4.c
@@ -1,24 +1,32 @@
 #include <unistd.h> 
 #include <stdio.h> 
+#include <string.h>
 #include <sys/wait.h> 
 #include <stdlib.h> 
 int main(int argc, char* argv[]) { 
 int pid; 
-char* arg_list[3];
-printf("Before: process id %d\n",getpid()); 
+char cmd[100];
+char* arg1;
+char* arg2;
 if ((pid = fork())==0){ 
-    printf("I am the child %d\n",getpid()); 
-    sleep(5); 
-    printf("Listing content of current directory...\n"); 
-    char* arg_list[3] = {"ls", "-l", (char *)0}; // OR // 
-    arg_list[0] = "ls"; // arg_list[1] = "-l"; // arg_list[2] = 0; 
-    execvp("ls",arg_list); } 
+    printf("> ");
+    fgets(cmd, 100, stdin);
+
+	int init_size = strlen(cmd);
+	char delim[] = " ";
+
+	char *ptr = strtok(cmd, delim);
+
+    arg1=ptr;
+    ptr = strtok(NULL, delim);
+    arg2=ptr;
+
+    char* arg_list[3] = {arg1, arg2, (char *)0};
+
+    execvp(cmd,arg_list); 
+} 
 else{ 
-    printf("I am the parent %d\n", getpid()); 
     int status; 
-    printf(">");
-    scanf("%s %s %s",arg_list[0],arg_list[1],arg_list[2]);
     int term_pid = wait(&status); 
-    printf("Child %d has listed the content of current directory\n", term_pid); 
     exit(1); } 
 } 
